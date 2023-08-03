f65c62cc493b20abf4f984fcb2f16a4189a56c44
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-03 09:57:22 +0600

    added employees page

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ const App = () => {
             </div>
             {activeMenu ? (<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
               <Sidebar/>
-            </div>): (<div className='w-0 dark: bg-secondary-dark-bg' > <Sidebar/></div>)}
-            <div className={`dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? "md:ml-72": "flex-2"}`}>
+            </div>) : (<div className='w-0  dark:bg-secondary-dark-bg' > <Sidebar/></div>)}
+            <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72": "flex-1"}`}>
               <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                 <Navbar/>
               </div>
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective,Page, Search,Inject, Toolbar} from '@syncfusion/ej2-react-grids'
+import { employeesData,contextMenuItems,employeesGrid} from '../data/dummy'
+import {Header} from "../components"
+import { search } from '@syncfusion/ej2/filemanager'
+
+
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders"/>
+      <GridComponent dataSource={employeesData} allowPaging toolbar={["Search"]} width="auto" >
+        <ColumnsDirective>
+         {employeesGrid.map((item,index)=>{
+             return <ColumnDirective {...item} key={index}/>
+         })}
+        </ColumnsDirective>
+        <Inject services={[Page,Search, Toolbar]}/>
+      </GridComponent>
+    </div>
   )
 }
 
